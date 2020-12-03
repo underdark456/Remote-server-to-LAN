89f3e41acc0eb12d5cfd8dcd69aa266bcf0f385d
underdark456 <31307789+underdark456@users.noreply.github.com>
2020-12-03 17:44:37 +0300

    Update remoteToLAN.sh

--- remoteToLAN.sh
+++ remoteToLAN.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash
+#RUN ON SERVER(Target NMS)
+logger "Telling kernel to turn on ipv4 ip_forwarding"
+echo 1 > /proc/sys/net/ipv4/ip_forward
+#Generate keys on SERVER(Target NMS)
+ssh-keygen -b 2048 -t rsa -f ~root/.ssh/id_rsa -q -N ""
+scp
